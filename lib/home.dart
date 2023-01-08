eafe7b8625e50616640ea7a78d123b4af959b143
rizkiaryandi <Aryandi30@gmail.com>
2023-01-08 20:49:20 +0700

    New

--- lib/home.dart
+++ lib/home.dart
@@ -89,45 +89,51 @@ class _HomePageState extends State<HomePage>
       child: Scaffold(
         appBar: AppBar(
           bottom: PreferredSize(
-              preferredSize: Size.fromHeight(40),
-              child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: TabBar(
-                    onTap: _changeSelectedNavBar,
-                    controller: _tabController,
-                    padding: EdgeInsets.only(left: 16),
-                    indicator: UnderlineTabIndicator(
-                        borderSide: BorderSide(
-                          width: 4,
-                          color: Color(0xFF646464),
-                        ),
-                        insets: EdgeInsets.only(left: 0, right: 8, bottom: 4)),
-                    tabs: [
-                      Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: Tab(text: "Tadarus")),
-                      Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: Tab(text: "Muroja'ah")),
-                      Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: Tab(text: "Analisa")),
-                    ],
-                    isScrollable: true,
-                    labelPadding: EdgeInsets.only(left: 0, right: 0),
-                  ))),
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              onPressed: () {
-                _openUserInfo();
-              },
-              icon: Icon(
-                Icons.person_outline,
-                size: 28,
+            preferredSize: Size.fromHeight(50),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: BottomNavyBar(
+                backgroundColor: Colors.transparent,
+                selectedIndex: _selectedNavbar,
+                showElevation: true, //
+                onItemSelected: (index) => {
+                  if (index < 3)
+                    {
+                      setState(() {
+                        _selectedNavbar = index;
+                        _tabController?.animateTo(_selectedNavbar);
+                      })
+                    }
+                  else
+                    {_openUserInfo()}
+                },
+                items: [
+                  BottomNavyBarItem(
+                    icon: Icon(Icons.book_outlined),
+                    title: Text('Tadarus'),
+                    activeColor: Colors.white,
+                  ),
+                  BottomNavyBarItem(
+                    icon: Icon(Icons.lightbulb_outline),
+                    title: Text('Murojaah'),
+                    activeColor: Colors.white,
+                  ),
+                  BottomNavyBarItem(
+                    icon: Icon(Icons.play_arrow),
+                    title: Text('Reels'),
+                    activeColor: Colors.white,
+                  ),
+                  BottomNavyBarItem(
+                    icon: Icon(Icons.person_outline),
+                    title: Text('Pengguna'),
+                    activeColor: Colors.white,
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
+          title: Text(widget.title),
+
           elevation: 0,
           backgroundColor: Colors.transparent,
           // bottomOpacity: 0.0,
@@ -138,45 +144,13 @@ class _HomePageState extends State<HomePage>
             TadarusWidget(),
             MurojaahWidget(),
             Container(
-              child: Center(child: Text("Segera Hadir fitur analisa Uiniqu")),
+              padding: EdgeInsets.all(20),
+              child: Center(
+                  child: Text(
+                "Segera hadir reels ayat-ayat Al Quran nantikan update terbarunya hanya di UINIQU",
+                textAlign: TextAlign.center,
+              )),
             )
-          ],
-        ),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _selectedNavbar,
-          showElevation: true, //
-          onItemSelected: (index) => {
-            if (index < 3)
-              {
-                setState(() {
-                  _selectedNavbar = index;
-                  _tabController?.animateTo(_selectedNavbar);
-                })
-              }
-            else
-              {_openUserInfo()}
-          },
-          items: [
-            BottomNavyBarItem(
-              icon: Icon(Icons.book_outlined),
-              title: Text('Tadarus'),
-              activeColor: Colors.white,
-            ),
-            BottomNavyBarItem(
-              icon: Icon(Icons.lightbulb_outline),
-              title: Text('Murojaah'),
-              activeColor: Colors.white,
-            ),
-            BottomNavyBarItem(
-              icon: Icon(Icons.bar_chart_outlined),
-              title: Text('Analisa'),
-              activeColor: Colors.white,
-            ),
-            BottomNavyBarItem(
-              icon: Icon(Icons.person_outline),
-              title: Text('Pengguna'),
-              activeColor: Colors.white,
-            ),
           ],
         ),
       ),
