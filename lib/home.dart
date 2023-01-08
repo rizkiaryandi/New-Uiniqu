6533f6246fdc00e359b952a8786d00489acb5078
rizkiaryandi <Aryandi30@gmail.com>
2023-01-08 16:42:34 +0700

    Add Tab

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
 //Pages
 import './pages/tadarus.dart';
@@ -17,7 +18,22 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
   _openRegister() {
     Future<void> future = showModalBottomSheet<void>(
         isScrollControlled: true,
@@ -54,55 +70,116 @@ class _HomePageState extends State<HomePage> {
         });
   }
 
+  int _selectedNavbar = 0;
+
+  void _changeSelectedNavBar(int index) {
+    if (index < 3) {
+      setState(() {
+        _selectedNavbar = index;
+      });
+    } else {
+      _openUserInfo();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-            appBar: AppBar(
-              bottom: PreferredSize(
-                  preferredSize: Size.fromHeight(40),
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: TabBar(
-                        padding: EdgeInsets.only(left: 16),
-                        indicator: UnderlineTabIndicator(
-                            borderSide: BorderSide(
-                              width: 4,
-                              color: Color(0xFF646464),
-                            ),
-                            insets:
-                                EdgeInsets.only(left: 0, right: 8, bottom: 4)),
-                        tabs: [
-                          Padding(
-                              padding: const EdgeInsets.only(right: 8),
-                              child: Tab(text: "Tadarus")),
-                          Padding(
-                              padding: const EdgeInsets.only(right: 8),
-                              child: Tab(text: "Muroja'ah")),
-                        ],
-                        isScrollable: true,
-                        labelPadding: EdgeInsets.only(left: 0, right: 0),
-                      ))),
-              title: Text(widget.title),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    _openUserInfo();
-                  },
-                  icon: Icon(
-                    Icons.person_outline,
-                    size: 28,
-                  ),
-                ),
-              ],
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-              // bottomOpacity: 0.0,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: PreferredSize(
+              preferredSize: Size.fromHeight(40),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TabBar(
+                    onTap: _changeSelectedNavBar,
+                    controller: _tabController,
+                    padding: EdgeInsets.only(left: 16),
+                    indicator: UnderlineTabIndicator(
+                        borderSide: BorderSide(
+                          width: 4,
+                          color: Color(0xFF646464),
+                        ),
+                        insets: EdgeInsets.only(left: 0, right: 8, bottom: 4)),
+                    tabs: [
+                      Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Tab(text: "Tadarus")),
+                      Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Tab(text: "Muroja'ah")),
+                      Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Tab(text: "Analisa")),
+                    ],
+                    isScrollable: true,
+                    labelPadding: EdgeInsets.only(left: 0, right: 0),
+                  ))),
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              onPressed: () {
+                _openUserInfo();
+              },
+              icon: Icon(
+                Icons.person_outline,
+                size: 28,
+              ),
             ),
-            body: TabBarView(children: [
-              TadarusWidget(),
-              MurojaahWidget(),
-            ])));
+          ],
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          // bottomOpacity: 0.0,
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            TadarusWidget(),
+            MurojaahWidget(),
+            Container(
+              child: Center(child: Text("Segera Hadir fitur analisa Uiniqu")),
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavyBar(
+          selectedIndex: _selectedNavbar,
+          showElevation: true, //
+          onItemSelected: (index) => {
+            if (index < 3)
+              {
+                setState(() {
+                  _selectedNavbar = index;
+                  _tabController?.animateTo(_selectedNavbar);
+                })
+              }
+            else
+              {_openUserInfo()}
+          },
+          items: [
+            BottomNavyBarItem(
+              icon: Icon(Icons.book_outlined),
+              title: Text('Tadarus'),
+              activeColor: Colors.white,
+            ),
+            BottomNavyBarItem(
+              icon: Icon(Icons.lightbulb_outline),
+              title: Text('Murojaah'),
+              activeColor: Colors.white,
+            ),
+            BottomNavyBarItem(
+              icon: Icon(Icons.bar_chart_outlined),
+              title: Text('Analisa'),
+              activeColor: Colors.white,
+            ),
+            BottomNavyBarItem(
+              icon: Icon(Icons.person_outline),
+              title: Text('Pengguna'),
+              activeColor: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
